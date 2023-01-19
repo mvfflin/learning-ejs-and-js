3fe9e583dce9a21aa7a76c1ef9cd0856e5403b7f
mvfflin <fatihulramu@gmail.com>
2023-01-19 20:10:08 +0700

    major updates and changes + added new package (dotenv, mongodb) + updates in server.js, index.ejs, list.ejs, and mainlayout.ejs + added .gitignore, mainlayout.css

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const env = require('./env')
 const app = express();
+
+// delay function
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+// end of delay function
+
+// database connect
+async function database() {
+
+    const {MongoClient} = require('mongodb');
+    const mongouri = env.uri;
+    const mongoclient = new MongoClient(mongouri)
+    await delay(5)
+
+    try {
+        await mongoclient.connect();
+        console.log('connected to db')
+        await listDatabases(mongoclient);
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await mongoclient.close();
+    }
+}
+
+database().catch(console.error);
+
+async function listDatabases(mongoclient) {
+    databaseList = await mongoclient.db().admin().listDatabases()
+
+    console.log("Databases:");
+    databaseList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+// end of database connect
 
 port = 9046;
 
@@ -14,10 +50,18 @@ app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.render('index', {
+        layout: 'layouts/mainlayout',
         title: 'hi!'
     })
 });
 
-app.get('/lmao', (req, res) => {
-    res.render('lmao.ejs')
+app.get('/list', (req, res) => {
+    res.render('list', {
+        layout: 'layouts/mainlayout',
+        title: 'user list'
+    })
 })
+
+app.get('/home', (req, res) => {
+    res.redirect('/')
+});
